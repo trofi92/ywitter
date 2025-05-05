2beed7796ec66e1fa5e0adeb080c911884a0f1d7
SY <lunaslash9249@gmail.com>
2025-05-05 16:00:41 +0900

    250505 Ywitter 웹앱 시작

---
+++ README.md
@@ -0,0 +1,5 @@
+#Ywitter
+
+X(ex,Twitter) (mini) Clone with React and Firebase
+
+250505 프로젝트 시작
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+ return (
+  <>
+   <div>HI</div>
+  </>
+ );
+}
+
+export default App;
